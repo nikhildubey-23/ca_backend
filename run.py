55d27f179398c6a58392c4b5601118e1ef883e77
nikhildubey-23 <ravannick0089@gmail.com>
2026-03-20 14:12:37 +0530

    railway

--- run.py
+++ run.py
@@ -1,13 +1,9 @@
-from app import create_app, db
-import os
-from models.user import User
-from models.document import Document
-from models.folder import Folder
-from models.tax_record import TaxRecord
+from app import app
+from extensions import db
 
-app = create_app()
 db.create_all()
 
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
